de83255dc270e24c0e403f738daed82a995990c7
Mya Mahaley <mahaleym2@go.lisd.net>
2017-05-19 10:52:53 -0500

    Hang

--- Hangman/Hangman/src/Hangman.java
+++ Hangman/Hangman/src/Hangman.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
@@ -8,6 +9,7 @@ import javax.swing.text.BadLocationException;
 public class Hangman
 {
 	int right, wrong;
+	ArrayList<ImageIcon> image = new ArrayList<ImageIcon>();
 	String level;
 	
 	public Hangman(String difficult)
@@ -20,8 +22,9 @@ public class Hangman
 	public JPanel getPanel()
 	{
 		JPanel p = new JPanel();
-		return p;
-		
-		
+		ImageIcon ninja1 = new ImageIcon("ninja.jpg");
+		JLabel nin = new JLabel(ninja1);
+		p.add(nin);
+		return p;		
 	}
 }
--- Hangman/Hangman/src/MainWindow.java
+++ Hangman/Hangman/src/MainWindow.java
@@ -12,6 +12,7 @@ public class MainWindow implements ActionListener
     
     private InstructionsPage ins = new InstructionsPage();
     private Board board = new Board();
+    private Hangman h;
     
     private JPanel game1, game2, game3, p, keyboard;
     private JButton b;
@@ -49,7 +50,7 @@ public class MainWindow implements ActionListener
         p.add(game2);
         p.add(game3);
         
-        level = "Easy";
+     //   level = "Easy";
 
         game1.setVisible(false);
         game2.setVisible(false);
