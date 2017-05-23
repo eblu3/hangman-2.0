a9fe7508b4429981b1c90feb1190f26af2faf2d1
Elliott Saltar <esaltar@gmail.com>
2017-05-23 11:15:58 -0500

    in progress: fixing display of word in-game

--- Hangman/Hangman/src/Board.java
+++ Hangman/Hangman/src/Board.java
@@ -1,25 +1,33 @@
 import java.awt.*;
+import java.awt.font.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.*;
 import javax.swing.*;
 
 public class Board implements ActionListener
 {
 	private ArrayList<JButton> buttons;
 	private ArrayList<JLabel> letters;
+	private ArrayList<JLabel> visibleLetters;
 	private JLabel word1, word2, gameover;
 	private JPanel keys;
 	private Difficulty d;
 	private String word;
 	private Hangman h;
+	private Font font = new Font("ChalkBoard", Font.BOLD,40);
+	private Map<TextAttribute, Object> map = new Hashtable<TextAttribute, Object>();
+	private JLabel let = new JLabel("", SwingConstants.CENTER);
 	boolean gameWon = true;
 	
 	public Board()
 	{
 		buttons = new ArrayList<JButton>();
 		letters = new ArrayList<JLabel>();
+		
+		map.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		font = font.deriveFont(map);
 		
 		for(int i = 65; i <= 90; i++) //Creating Alphabet Letters
 		{
@@ -33,6 +41,14 @@ public class Board implements ActionListener
 		d = new Difficulty("easy");
 		h = new Hangman("easy");
 		word = d.getPhrase();
+		
+		for(int i = 0; i < word.length(); i++)
+		{
+			JLabel add = new JLabel(word.substring(i, i+1), SwingConstants.CENTER);
+			add.setFont(font);
+			letters.add(add);
+		}
+		
 		System.out.println(word);
 		
 		JPanel game = new JPanel(new GridLayout()); //Game Consel
@@ -52,6 +68,12 @@ public class Board implements ActionListener
 		d = new Difficulty("medium");
 		word = d.getPhrase();
 		h = new Hangman("medium");
+		
+		for(int i = 0; i < word.length(); i++)
+		{
+			letters.add(new JLabel(word.substring(i, i+1)));
+		}
+		
 		System.out.println(word);
 		 
 		JPanel game = new JPanel(new GridLayout()); //Game Consel
@@ -71,6 +93,12 @@ public class Board implements ActionListener
 		d = new Difficulty("hard");
 		h = new Hangman("hard");
 		word = d.getPhrase();
+		
+		for(int i = 0; i < word.length(); i++)
+		{
+			letters.add(new JLabel(word.substring(i, i+1)));
+		}
+		
 		System.out.println(word);
 		 
 		JPanel game = new JPanel(new GridLayout()); //Game Consel
@@ -88,47 +116,21 @@ public class Board implements ActionListener
 	private JPanel hangmanBox()
 	{
 		JPanel p = new JPanel(new GridLayout(3,word.length() - 1));
-		word1 = new JLabel("Game");
-		word2 = new JLabel("Won");
-		word1.setFont(new Font("ChalkBoard", Font.BOLD, 20));
-		word2.setFont(new Font("ChalkBoard", Font.BOLD, 20));
-		
-		for(int i =0; i < word.length() -1; i++)
-		{
-			if(i == (word.length() / 2) -1)
-				p.add(word1);
-			if(i == (word.length() / 2))
-				p.add(word2);
-			else
-				p.add(new JLabel());
-		}
-		
-		word1.setVisible(false);
-		word2.setVisible(false);
 		
 		p.setPreferredSize(new Dimension(150,150));
 		p.setBackground(new Color(234, 124, 110));
-	
-		for(int i =0; i < word.length(); i++)
-		{
-			JLabel let = new JLabel(word.substring(i,i+1)); 
-			let.setFont(new Font("ChalkBoard", Font.BOLD, 40));
-			let.setPreferredSize(new Dimension(5,5)); 
-			letters.add(let);
 		
-			p.add(let);
-			let.setVisible(false);
+		visibleLetters = (ArrayList<JLabel>) letters.clone();
 		
-		}
+		let.setFont(font);
 	
-		for(int i = 0; i < word.length(); i++)
+		for(int i =0; i < letters.size(); i++)
 		{
-			JLabel blank = new JLabel("_"); 
-			blank.setFont(new Font("ChalkBoard", Font.BOLD,40));
-			blank.setPreferredSize(new Dimension(5,5));
-			p.add(blank); 
+			
 		}
-	
+		
+		p.add(let);
+		
 		return p;
 	}
 	
@@ -160,408 +162,99 @@ public class Board implements ActionListener
 	}
 	public void actionPerformed(ActionEvent e) 
 	{
-		if(e.getActionCommand().equals("A"))
-		{	
-			if(word.indexOf("a") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("a") >= 0)
-			{
-				letters.get(word.indexOf("a")).setVisible(true);
-				word = word.substring(0, word.indexOf("a")) + "!" + word.substring(word.indexOf("a") + 1);
-			}
-			buttons.get(0).setVisible(false);	
-		}
-	
-		if(e.getActionCommand().equals("B"))
+		String input = e.getActionCommand().toLowerCase();
+		
+		if(word.indexOf(input) < 0)
 		{
-			if(word.indexOf("b") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("b") >= 0)
-			{
-				letters.get(word.indexOf("b")).setVisible(true);
-				word = word.substring(0, word.indexOf("b")) + "!" + word.substring(word.indexOf("b") + 1);
-			}	
-			
+			h.addWrong();
+			h.changeImage();
+		}
+		
+		while(word.indexOf(input) >= 0)
+		{
+			letters.get(word.indexOf(input)).setVisible(false);
+			word = word.substring(0, word.indexOf(input)) + "!" + word.substring(word.indexOf(input)+1);
+		}
+		
+		switch(input)
+		{
+		case "a":
+			buttons.get(0).setVisible(false);
+			break;
+		case "b":
 			buttons.get(1).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("C"))
-		{	
-			if(word.indexOf("c") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("c") >= 0)
-			{
-				letters.get(word.indexOf("c")).setVisible(true);
-				word = word.substring(0, word.indexOf("c")) + "!" + word.substring(word.indexOf("c") + 1);
-			}	
+			break;
+		case "c":
 			buttons.get(2).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("D"))
-		{	
-			if(word.indexOf("d") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("d") >= 0)
-			{
-				letters.get(word.indexOf("d")).setVisible(true);
-				word = word.substring(0, word.indexOf("d")) + "!" + word.substring(word.indexOf("d") + 1);
-			}	
-			
+			break;
+		case "d":
 			buttons.get(3).setVisible(false);
-		
-		}
-		
-		if(e.getActionCommand().equals("E"))
-		{	
-			if(word.indexOf("e") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("e") >= 0)
-			{
-				letters.get(word.indexOf("e")).setVisible(true);
-				word = word.substring(0, word.indexOf("e")) + "!" + word.substring(word.indexOf("e") + 1);
-			 
-			}	
+			break;
+		case "e":
 			buttons.get(4).setVisible(false);
-			
-		}
-		
-		if(e.getActionCommand().equals("F"))
-		{	
-			if(word.indexOf("f") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("f") >= 0)
-			{
-				letters.get(word.indexOf("f")).setVisible(true);
-				word = word.substring(0, word.indexOf("f")) + "!" + word.substring(word.indexOf("f") + 1);
-			}	
+			break;
+		case "f":
 			buttons.get(5).setVisible(false);
-		
-		}
-		
-		if(e.getActionCommand().equals("G"))
-		{	
-			if(word.indexOf("g") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("g") >= 0)
-			{
-				letters.get(word.indexOf("g")).setVisible(true);
-				word = word.substring(0, word.indexOf("g")) + "!" + word.substring(word.indexOf("g") + 1);
-			}	
+			break;
+		case "g":
 			buttons.get(6).setVisible(false);
-		
-		}
-		
-		if(e.getActionCommand().equals("H"))
-		{	
-			if(word.indexOf("h") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("h") >= 0)
-			{
-				letters.get(word.indexOf("h")).setVisible(true);
-				word = word.substring(0, word.indexOf("h")) + "!" + word.substring(word.indexOf("h") + 1);
-			}	
-			buttons.get(7).setVisible(false);	
-		}
-		
-		if(e.getActionCommand().equals("I"))
-		{	
-			if(word.indexOf("i") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("i") >= 0)
-			{
-				letters.get(word.indexOf("i")).setVisible(true);
-				word = word.substring(0, word.indexOf("i")) + "!" + word.substring(word.indexOf("i") + 1);		 
-			}			
-			buttons.get(8).setVisible(false);		
-		}
-		
-		if(e.getActionCommand().equals("J"))
-		{	
-			if(word.indexOf("j") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("j") >= 0)
-			{
-				letters.get(word.indexOf("j")).setVisible(true);
-				word = word.substring(0, word.indexOf("j")) + "!" + word.substring(word.indexOf("j") + 1);
-			}	
+			break;
+		case "h":
+			buttons.get(7).setVisible(false);
+			break;
+		case "i":
+			buttons.get(8).setVisible(false);
+			break;
+		case "j":
 			buttons.get(9).setVisible(false);
-		
-		}
-		
-		if(e.getActionCommand().equals("K"))
-		{	
-			if(word.indexOf("k") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("k") >= 0)
-			{
-				letters.get(word.indexOf("k")).setVisible(true);
-				word = word.substring(0, word.indexOf("k")) + "!" + word.substring(word.indexOf("k") + 1);
-			}	
+			break;
+		case "k":
 			buttons.get(10).setVisible(false);
-		
-		}
-		
-		if(e.getActionCommand().equals("L"))
-		{	
-			if(word.indexOf("l") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("l") >= 0)
-			{
-				letters.get(word.indexOf("l")).setVisible(true);
-				word = word.substring(0, word.indexOf("l")) + "!" + word.substring(word.indexOf("l") + 1);
-			}	
-				buttons.get(11).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("M"))
-		{	
-			if(word.indexOf("m") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("m") >= 0)
-			{
-				letters.get(word.indexOf("m")).setVisible(true);
-				word = word.substring(0, word.indexOf("m")) + "!" + word.substring(word.indexOf("m") + 1);
-			}	
+			break;
+		case "l":
+			buttons.get(11).setVisible(false);
+			break;
+		case "m":
 			buttons.get(12).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("N"))
-		{	
-			if(word.indexOf("n") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("n") >= 0)
-			{
-				letters.get(word.indexOf("n")).setVisible(true);
-				word = word.substring(0, word.indexOf("n")) + "!" + word.substring(word.indexOf("n") + 1);
-			}	
+			break;
+		case "n":
 			buttons.get(13).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("O"))
-		{	
-			if(word.indexOf("o") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("o") >= 0)
-			{
-				letters.get(word.indexOf("o")).setVisible(true);
-				word = word.substring(0, word.indexOf("o")) + "!" + word.substring(word.indexOf("o") + 1);
-		 
-			}	
+			break;
+		case "o":
 			buttons.get(14).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("P"))
-		{	
-			if(word.indexOf("p") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("p") >= 0)
-			{
-			letters.get(word.indexOf("p")).setVisible(true);
-			word = word.substring(0, word.indexOf("p")) + "!" + word.substring(word.indexOf("p") + 1);
-			}	
+			break;
+		case "p":
 			buttons.get(15).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("Q"))
-		{	
-			if(word.indexOf("q") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("q") >= 0)
-			{
-				letters.get(word.indexOf("q")).setVisible(true);
-				word = word.substring(0, word.indexOf("q")) + "!" + word.substring(word.indexOf("q") + 1);
-		}	
-		buttons.get(16).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("R"))
-		{	
-			if(word.indexOf("r") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("r") >= 0)
-			{
-					letters.get(word.indexOf("r")).setVisible(true);
-					word = word.substring(0, word.indexOf("r")) + "!" + word.substring(word.indexOf("r") + 1);
-			}	
+			break;
+		case "q":
+			buttons.get(16).setVisible(false);
+			break;
+		case "r":
 			buttons.get(17).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("S"))
-		{	
-			if(word.indexOf("s") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("s") >= 0)
-			{
-				letters.get(word.indexOf("s")).setVisible(true);
-				word = word.substring(0, word.indexOf("s")) + "!" + word.substring(word.indexOf("s") + 1);
-			}	
+			break;
+		case "s":
 			buttons.get(18).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("T"))
-		{
-			if(word.indexOf("t") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("t") >= 0)
-			{
-				letters.get(word.indexOf("t")).setVisible(true);
-				word = word.substring(0, word.indexOf("t")) + "!" + word.substring(word.indexOf("t") + 1);
-			}	
+			break;
+		case "t":
 			buttons.get(19).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("U"))
-		{	
-			if(word.indexOf("u") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("u") >= 0)
-			{
-				letters.get(word.indexOf("u")).setVisible(true);
-				word = word.substring(0, word.indexOf("u")) + "!" + word.substring(word.indexOf("u") + 1);
-		 
-			}	
+			break;
+		case "u":
 			buttons.get(20).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("V"))
-		{
-			if(word.indexOf("v") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("v") >= 0)
-			{
-				letters.get(word.indexOf("v")).setVisible(true);
-				word = word.substring(0, word.indexOf("v")) + "!" + word.substring(word.indexOf("v") + 1);
-			}	
+			break;
+		case "v":
 			buttons.get(21).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("W"))
-		{
-			if(word.indexOf("w") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("w") >= 0)
-			{
-				letters.get(word.indexOf("w")).setVisible(true);
-				word = word.substring(0, word.indexOf("w")) + "!" + word.substring(word.indexOf("w") + 1);
-			}	
-				buttons.get(22).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("X"))
-		{			
-			if(word.indexOf("x") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("x") >= 0)
-			{
-				letters.get(word.indexOf("x")).setVisible(true);
-				word = word.substring(0, word.indexOf("x")) + "!" + word.substring(word.indexOf("x") + 1);
-			}	
-			
-			
+			break;
+		case "w":
+			buttons.get(22).setVisible(false);
+			break;
+		case "x":
 			buttons.get(23).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("Y"))
-		{	
-			if(word.indexOf("y") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("y") >= 0)
-			{
-				letters.get(word.indexOf("y")).setVisible(true);
-				word = word.substring(0, word.indexOf("y")) + "!" + word.substring(word.indexOf("y") + 1);
-			}	
+			break;
+		case "y":
 			buttons.get(24).setVisible(false);
-		}
-		
-		if(e.getActionCommand().equals("Z"))
-		{
-			if(word.indexOf("z") < 0)
-			{
-				h.addWrong();
-				h.changeImage();
-			}
-			while(word.indexOf("z") >= 0)
-			{
-				letters.get(word.indexOf("z")).setVisible(true);
-				word = word.substring(0, word.indexOf("z")) + "!" + word.substring(word.indexOf("z") + 1);
-			}	
+			break;
+		case "z":
 			buttons.get(25).setVisible(false);
-		
 		}
 		
 		for(JLabel l:letters)
@@ -572,8 +265,6 @@ public class Board implements ActionListener
 		
 		if(gameWon == true)
 		{
-			word1.setVisible(true);
-			word2.setVisible(true);
 			System.out.println("game won");
 		}
 		
@@ -593,5 +284,4 @@ public class Board implements ActionListener
 		
 		gameWon = true;
 	}
-
 }
